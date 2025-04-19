c516cb96dd5fcb28ea6b9dc89927fa043f7a9206
Nishan-k <karki.nishann@gmail.com>
2025-04-19 14:11:35 +0200

    Changes to PDF generator

--- src/navigation_pages/about.py
+++ src/navigation_pages/about.py
@@ -3,9 +3,10 @@ import streamlit as st
 
 
 
-def about():
-
+def about():    
     st.markdown("""
-                 A database was initially used for data ingestion. 
-                  For deployment, I switched to a static CSV for simplicity. 
-                  A database version with prediction logging and retraining functionality is in progress. """)
+                <p style='font-size:20px;'> 
+                A database was initially used for data ingestion. For deployment, I switched to a static CSV for simplicity. 
+                A database version with prediction logging and retraining functionality is in progress.
+                </p>""", 
+                unsafe_allow_html=True)
